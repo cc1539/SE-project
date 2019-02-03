d72fd20c3740760a0e9d6b8e65b2d1b5ee02bb11
cc1539 <43189993+cc1539@users.noreply.github.com>
2019-02-02 20:09:56 -0500

    Add files via upload

---
+++ se_project_2.js
@@ -0,0 +1,110 @@
+
+class Part {
+  
+  constructor(x,y,vx,vy,radius) {
+    this.x = x;
+    this.y = y;
+    this.px = x-vx;
+    this.py = y-vy;
+    this.radius = radius;
+  }
+  
+  move() {
+    this.x += -this.px+(this.px=this.x);
+    this.y += -this.py+(this.py=this.y);
+  }
+  
+  draw() {
+    var diameter = this.radius*2;
+    noStroke();
+    fill(255);
+    ellipse(this.x,this.y,diameter,diameter);
+  }
+  
+  interact(part) {
+    var dx = this.x - part.x;
+    var dy = this.y - part.y;
+    if(dx!=0 || dy!=0) {
+      var dst2 = dx*dx+dy*dy;
+      var rads = this.radius+part.radius;
+      if(dst2<rads*rads) {
+        var force = (1-rads/sqrt(dst2)) * .5;
+        dx *= force;
+        dy *= force;
+        part.x += dx;
+        part.y += dy;
+        this.x -= dx;
+        this.y -= dy;
+      }
+    }
+  }
+  
+}
+
+var parts = [];
+
+function setup() {
+  createCanvas(window.innerWidth,window.innerHeight);
+}
+
+function keyPressed() {
+  switch(key) {
+    case 'n': {
+      // add a part
+      parts.push(new Part(
+        mouseX,
+        mouseY,
+        mouseX-pmouseX,
+        mouseY-pmouseY,
+        20));
+    } break;
+    case 'c': {
+      // remove all parts
+      parts.length = 0;
+    } break;
+  }
+}
+
+function mousePressed() {
+  parts.push(new Part(
+    mouseX,
+    mouseY,
+    mouseX-pmouseX,
+    mouseY-pmouseY,
+    20));
+}
+
+function draw() {
+  background(0);
+  
+  // apply collisions between all particles
+  for(var i=0;i<parts.length;i++) {
+  for(var j=i+1;j<parts.length;j++) {
+    parts[i].interact(parts[j]);
+  }
+  }
+  
+  // draw all particles
+  for(var i=0;i<parts.length;i++) {
+    parts[i].draw();
+  }
+  
+  // allow all particles to move using verlet integration
+  for(var i=0;i<parts.length;i++) {
+    
+    parts[i].py -= .1; // apply a little gravity
+    
+    parts[i].move();
+    
+    // confine parts to the canvas
+    var radius = parts[i].radius;
+    parts[i].x = min(max(parts[i].x,radius),width-radius);
+    parts[i].y = min(max(parts[i].y,radius),height-radius);
+    
+  }
+  
+}
+
+function windowResized() {
+  resizeCanvas(window.innerWidth,window.innerHeight);
+}
